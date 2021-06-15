c72802630557d2bc132b407d2d54cae008784234
Facundo Torres <ftorres@ubicuagroup.com>
2021-06-15 19:18:24 -0300

    refactoring

---
+++ Ahorcado/HangmanController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ahorcado
+{
+    public class HangmanController
+    {
+        private Hangman hangman { get; set; }
+        private User sessionUser { get; set; }
+
+        public User Login(string username)
+        {
+            sessionUser = new User(username);
+            return sessionUser;
+        }
+
+        public Hangman StartGame(string word)
+        {
+            hangman = new Hangman(word);
+            int position = 0;
+            foreach (char letter in hangman.WordToGuess.ToCharArray())
+            {
+                position++;
+                hangman.Letters.Add(new HangmanLetter(position, letter, false));
+            }
+            return hangman;
+        }
+
+        public Hangman EnterLetter(char letter)
+        {
+            if (ValidateLetter(letter))
+            {
+                IEnumerable<HangmanLetter> foundLetters = hangman.Letters.Where(x => x.Letter == letter);
+                foreach (HangmanLetter item in foundLetters)
+                {
+                    item.Display = true;
+                }
+                hangman.GuessedTheLetter = true;
+                return hangman;
+            }
+            if (!hangman.BadLetters.Contains(letter))
+            {
+                hangman.BadLetters.Add(letter);
+                hangman.Lifes -= 1;
+            }
+            hangman.GuessedTheLetter = false;
+            return hangman;
+        }
+
+        public Hangman EnterWord(string word)
+        {
+            if (hangman.WordToGuess == word.ToUpper())
+            {
+                foreach (HangmanLetter item in hangman.Letters)
+                {
+                    item.Display = true;
+                }
+                hangman.GuessedWholeWord = true;
+            } else
+            {
+                hangman.GuessedWholeWord = false;
+                hangman.Lifes -= 2;
+            }
+            return hangman;
+        }
+
+        public int GetPoints()
+        {
+            if (hangman.Lifes <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int extraPoints = 0;
+                if (hangman.GuessedWholeWord)
+                {
+                    extraPoints = Constant.POINTS_PER_WORD;
+                }
+                return Constant.POINTS_FOR_WINNING - (hangman.BadLetters.Count * Constant.POINTS_LESS_BY_MISTAKE) + extraPoints;
+            }
+        }
+
+        private bool ValidateLetter(char letter)
+        {
+            if (!Char.IsLetter(letter))
+            {
+                throw new ArgumentException("El caracter ingresado no es una letra.");
+            }
+            return hangman.WordToGuess.Contains(Char.ToUpper(letter));
+        }
+    }
+}
---
+++ TestAhorcado/HangmanTest.cs
@@ -0,0 +1,313 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Ahorcado.Test
+{
+    public class HangmanTest
+    {
+        [Fact]
+        public void test_startgame()
+        {
+            HangmanController controller = new HangmanController();
+            
+            Hangman result = controller.StartGame("AUTOMATICO");
+
+            Assert.Equal("_ _ _ _ _ _ _ _ _ _", result.PrintWord());
+        }
+
+        [Fact]
+        public void test_enter_correct_letter()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            Hangman result = controller.EnterLetter('A');
+
+            Assert.True(result.GuessedTheLetter);
+        }
+
+        [Fact]
+        public void test_enter_non_correct_letter()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            Hangman result = controller.EnterLetter('H');
+
+            Assert.False(result.GuessedTheLetter);
+        }
+
+        [Fact]
+        public void test_enter_correct_letter_casesensitive()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            Hangman result = controller.EnterLetter('a');
+
+            Assert.True(result.GuessedTheLetter);
+        }
+
+        [Fact]
+        public void test_enter_non_letter_number()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => controller.EnterLetter('2'));
+
+            Assert.Equal("El caracter ingresado no es una letra.", ex.Message);
+        }
+
+        [Fact]
+        public void test_enter_non_letter_space()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => controller.EnterLetter(' '));
+
+            Assert.Equal("El caracter ingresado no es una letra.", ex.Message);
+        }
+
+        [Fact]
+        public void test_enter_non_letter_symbol()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => controller.EnterLetter('$'));
+
+            Assert.Equal("El caracter ingresado no es una letra.", ex.Message);
+        }
+
+        [Fact]
+        public void test_correct_letter_print_word()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            Hangman result = controller.EnterLetter('A');
+
+            Assert.Equal("A _ _ _ _ A _ _ _ _", result.PrintWord());
+        }
+
+        [Fact]
+        public void test_correct_guessed_word()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            Hangman result = controller.EnterWord("AUTOMATICO");
+
+            Assert.Equal("A U T O M A T I C O", result.PrintWord());
+        }
+        [Fact]
+        public void test_incorrect_guessed_word()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            Hangman result = controller.EnterWord("AUTOMATICA");
+
+            Assert.False(result.GuessedWholeWord);
+        }
+
+        [Fact]
+        public void test_incorrect_letters()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+            List<char> list = new List<char> { 'G', 'Q', 'P' };
+
+            controller.EnterLetter('G');
+            controller.EnterLetter('Q');
+            Hangman result = controller.EnterLetter('P');
+            
+            Assert.Equal(list, result.BadLetters);
+        }
+
+        [Fact]
+        public void test_incorrect_letters_duplicates()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+            List<char> list = new List<char> { 'G', 'Q', 'P' };
+
+            controller.EnterLetter('G');
+            controller.EnterLetter('Q');
+            controller.EnterLetter('Q');
+            controller.EnterLetter('P');
+            Hangman result = controller.EnterLetter('P');
+
+            Assert.Equal(list, result.BadLetters);
+        }
+
+        [Fact]
+        public void test_subtract_lifes()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            controller.EnterLetter('G');
+            controller.EnterLetter('Q');
+            Hangman result = controller.EnterLetter('P');
+
+            Assert.Equal(Constant.LIFES - 3, result.Lifes);
+        }
+
+        [Fact]
+        public void test_subtract_lifes_duplicates()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            controller.EnterLetter('G');
+            controller.EnterLetter('Q');
+            controller.EnterLetter('Q');
+            controller.EnterLetter('P');
+            Hangman result = controller.EnterLetter('P');
+
+            Assert.Equal(Constant.LIFES - 3, result.Lifes);
+        }
+
+        [Fact]
+        public void test_get_points()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            controller.EnterLetter('A');
+            controller.EnterLetter('U');
+            controller.EnterLetter('T');
+            controller.EnterLetter('O');
+            controller.EnterLetter('M');
+            controller.EnterLetter('I');
+            controller.EnterLetter('C');
+
+            Assert.Equal(Constant.POINTS_FOR_WINNING, controller.GetPoints());
+        }
+
+        [Fact]
+        public void test_get_points_incorrects()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            controller.EnterLetter('A');
+            controller.EnterLetter('D');
+            controller.EnterLetter('X');
+            controller.EnterLetter('U');
+            controller.EnterLetter('T');
+            controller.EnterLetter('O');
+            controller.EnterLetter('M');
+            controller.EnterLetter('I');
+            Hangman result = controller.EnterLetter('C');
+
+            Assert.Equal(Constant.POINTS_FOR_WINNING - (Constant.POINTS_LESS_BY_MISTAKE * result.BadLetters.Count), controller.GetPoints());
+        }
+
+        [Fact]
+        public void test_get_points_all_incorrects()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            controller.EnterLetter('D');
+            controller.EnterLetter('Q');
+            controller.EnterLetter('P');
+            controller.EnterLetter('X');
+            controller.EnterLetter('L');
+            controller.EnterLetter('R');
+            controller.EnterLetter('Z');
+            controller.EnterLetter('Ñ');
+
+            Assert.Equal(0, controller.GetPoints());
+        }
+
+        [Fact]
+        public void test_get_points_1correct_rest_incorrects()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            controller.EnterLetter('O');
+            controller.EnterLetter('A');
+            controller.EnterLetter('D');
+            controller.EnterLetter('Q');
+            controller.EnterLetter('P');
+            controller.EnterLetter('X');
+            controller.EnterLetter('L');
+            controller.EnterLetter('R');
+            controller.EnterLetter('Z');
+            controller.EnterLetter('Ñ');
+
+            Assert.Equal(0, controller.GetPoints());
+        }
+
+        [Fact]
+        public void test_guessed_word_points()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            Hangman result = controller.EnterWord("AUTOMATICO");
+
+            Assert.Equal(Constant.POINTS_FOR_WINNING - (Constant.POINTS_LESS_BY_MISTAKE * result.BadLetters.Count) + Constant.POINTS_PER_WORD, controller.GetPoints());
+        }
+
+        [Fact]
+        public void test_guessed_word_w_errors_points()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            controller.EnterLetter('P');
+            controller.EnterLetter('E');
+            controller.EnterLetter('R');
+            Hangman result = controller.EnterWord("AUTOMATICO");
+
+            Assert.Equal(Constant.POINTS_FOR_WINNING - (Constant.POINTS_LESS_BY_MISTAKE * result.BadLetters.Count) + Constant.POINTS_PER_WORD, controller.GetPoints());
+        }
+
+        [Fact]
+        public void test_guessed_word_w_corrects_points()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            controller.EnterLetter('A');
+            controller.EnterLetter('U');
+            controller.EnterLetter('T');
+            Hangman result = controller.EnterWord("AUTOMATICO");
+
+            Assert.Equal(Constant.POINTS_FOR_WINNING - (Constant.POINTS_LESS_BY_MISTAKE * result.BadLetters.Count) + Constant.POINTS_PER_WORD, controller.GetPoints());
+        }
+
+        [Fact]
+        public void test_guessed_word_w_corrects_and_errors_points()
+        {
+            HangmanController controller = new HangmanController();
+            controller.StartGame("AUTOMATICO");
+
+            controller.EnterLetter('A');
+            controller.EnterLetter('U');
+            controller.EnterLetter('D');
+            controller.EnterLetter('T');
+            Hangman result = controller.EnterWord("AUTOMATICO");
+
+            Assert.Equal(Constant.POINTS_FOR_WINNING - (Constant.POINTS_LESS_BY_MISTAKE * result.BadLetters.Count) + Constant.POINTS_PER_WORD, controller.GetPoints());
+        }
+
+        [Fact]
+        public void test_enter_user_name()
+        {
+            HangmanController controller = new HangmanController();
+
+            User result = controller.Login("user1");
+
+            Assert.Equal("user1", result.Username);
+        }
+    }
+}
